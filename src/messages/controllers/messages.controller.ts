90579d26a5d4f37de9c2fd05ac09e4de8dee93f4
f.shojaeimehr <farisshojaemehr@gmail.com>
2023-11-22 19:09:55 +0330

    add get mthod

--- src/messages/controllers/messages.controller.ts
+++ src/messages/controllers/messages.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from "@nestjs/common";
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get()
+  public getMessages(){
+    return 'Hi'
+  }
+}
